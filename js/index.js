fa04327fa9f32641c3bfa3672eb3b95b68da452a
Luis Dejesus <luisdejesus@Luiss-MacBook-Air.local>
2020-06-22 12:06:33 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,65 @@
+// Sprite generator: http://gaurav.munjal.us/Universal-LPC-Spritesheet-Character-Generator/
+/******************** Global Variables ********************/
+const canvas = document.querySelector('#canvas')
+const cxt = canvas.getContext('2d')
+let sprite = new Image()
+sprite.src = '../images/sprite.png'
+let xPositionSprite = 0
+let yPositionSprite = 0
+let imageX = 0
+let imageY = 64*11
+let animationID;
+
+let canvasX = 0
+
+/******************** Functions ********************/
+function animationLoop() {
+    animationID = window.requestAnimationFrame(animationLoop)
+    cxt.clearRect(0, 0, canvas.width, canvas.height)
+    cxt.drawImage(sprite, imageX, imageY, 64, 64, xPositionSprite, yPositionSprite, 64, 64) // (imageObj, imageX, imageY, imageWidth, imageHeight, xCanvas, yCanvas, widthCanvas, heightCanvas)
+}
+
+function playerMove(e) {
+    if(e.key === 'ArrowRight') {
+        imageY = 64*11
+        xPositionSprite += 5
+        if(imageX + 64 >= 64*9) {
+            imageX = 0
+        } else {
+            imageX += 64
+        }
+    }
+    if(e.key === 'ArrowLeft') {
+        imageY = 64*9
+        xPositionSprite -= 5
+        if(imageX + 64 >= 64*9) {
+            imageX = 0
+        } else {
+            imageX += 64
+        }
+    }
+    if(e.key === 'ArrowDown') {
+        console.log('downnnnnnn')
+        yPositionSprite += 5
+        imageY = 64*10
+        if(imageX + 64 >= 64*9) {
+            imageX = 0
+        } else {
+            imageX += 64
+        }
+    }
+    if(e.key === 'ArrowUp') {
+        yPositionSprite -= 5
+        imageY = 64*8
+        if(imageX + 64 >= 64*9) {
+            imageX = 0
+        } else {
+            imageX += 64
+        }
+    }
+}
+
+
+/******************** Script ********************/
+document.onkeydown = playerMove
+animationLoop()
